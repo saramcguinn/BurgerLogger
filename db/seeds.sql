25eb88732edd246f04acbd69887fb5dd54a78608
sara.mcguinn <sara.mcguinn@gmail.com>
2020-10-03 17:10:27 -0500

    DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Whiskey King');
+INSERT INTO burgers (burger_name) VALUES ('Roquefort');
+INSERT INTO burgers (burger_name) VALUES ('The Lola');
